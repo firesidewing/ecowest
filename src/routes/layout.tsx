139d2d03b1156dfa7909d97cbcf3f2e1c444b3e0
Josh Dollinger <32228811+firesidewing@users.noreply.github.com>
2023-06-11 21:26:00 -0700

    remove header/footer

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,15 +1,13 @@
 import { component$, Slot } from '@builder.io/qwik';
-import Header from '../components/header/header';
-import Footer from '../components/footer/footer';
 
 export default component$(() => {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <main>
         <Slot />
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 });
